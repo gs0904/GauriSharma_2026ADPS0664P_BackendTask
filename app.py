2d58471ecc14783ecec889786fb52d41ac0e46b5
gs0904 <f20260664@pilani.bits-pilani.ac.in>
2026-09-24 15:09:14 +0530

    Set up Flask backend

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify
+import sqlite3
 
 app = Flask(__name__)
+
+
+def get_db_connection():
+    connection = sqlite3.connect("expense_tracker.db")
+    connection.row_factory = sqlite3.Row
+    return connection
 
 
 @app.route("/")
